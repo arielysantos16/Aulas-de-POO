9cb6bc996e5b84df3b533a5fe16822d9e3b9fcc9
Ariely <arielysantos156@gmail.com>
2026-07-06 15:53:26 -0300

    Update listas.py

--- revisaoAED1/listas.py
+++ revisaoAED1/listas.py
@@ -1,1 +1,17 @@
+#questão 01
+valores = [int(input("Informe uma lista de números inteiros, com pelo menos 4 valores:"))]
+print(f"Lista original: {valores}.")
 
+print("Lista dos 3 primeiros elementos:", valores[:3])
+
+print("Lista dos 2 últimos elementos:", valores[1:])#
+
+print("Lista invertida:", reverse(valores))
+
+print("Lista dos elementos de índice par:", valores[::2])
+
+indices_impares = []
+for indice in range(len(valores)):
+    if (indice/2) != 0:
+        indices_impares.append()
+    print(f"Lista dos elementos de índice ímpar: {indices_impares}.")
